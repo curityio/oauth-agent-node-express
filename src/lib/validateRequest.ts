6e53ec3fa48d556ead569f63489364a595cf58da
gary.archer <gary.archer@WORKMAC.local>
2021-07-27 14:16:17 +0200

    Updates during integration

--- src/lib/validateRequest.ts
+++ src/lib/validateRequest.ts
@@ -2,6 +2,8 @@ import {InvalidRequestException} from './exceptions'
 import {decryptCookie} from './cookieEncrypter'
 
 export default function validateRequest(csrfHeader: string | undefined, csrfCookie: string | undefined, originHeader: string | undefined, allowedOrigins: string[], encKey: string) {
+    
+    // GJA. I would more clearly include the cause of the exception for logging purposes
     if (allowedOrigins.findIndex((value) => value === originHeader) == -1) {
         throw new InvalidRequestException()
     }
