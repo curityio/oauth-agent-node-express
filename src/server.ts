92c79e4a0c668eb60c03275a75e1a32dba56b0b3
gary.archer <gary.archer@WORKMAC.local>
2021-08-03 13:27:26 +0200

    Fixed BFF endpoints path

--- src/server.ts
+++ src/server.ts
@@ -51,7 +51,7 @@ const controllers = {
 }
 
 for (const [path, controller] of Object.entries(controllers)) {
-    app.use(path, controller.router)
+    app.use(config.bffEndpointsPrefix + path, controller.router)
 }
 
 const server = app.listen(port, function() {
