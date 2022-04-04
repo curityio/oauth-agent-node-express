8022811f7fbfde4caaa4ee5c17e31c5f454604dc
gary.archer <gary.archer@WORKMAC.local>
2022-04-04 19:05:01 +0200

    Updates

--- test/integration/claimsControllerTests.ts
+++ test/integration/claimsControllerTests.ts
@@ -55,4 +55,4 @@ describe('ClaimsController', () => {
         const body = await response.json()
         console.log(body)
     })*/
-});
+})
---
+++ test/integration/loginControllerTests.ts
@@ -0,0 +1,12 @@
+import * as assert from 'assert'
+import fetch from 'node-fetch';
+import {config} from '../../src/config';
+
+describe('ClaimsController', () => {
+
+    const baseUrl = `http://localhost:${config.port}${config.endpointsPrefix}`
+
+    it('TODO: login tests', async () => {
+        console.log('*** here')
+    })
+})
