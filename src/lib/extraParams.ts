784136188adb680a6a8a2ab39d67cec57cecb5aa
gary.archer <gary.archer@WORKMAC.local>
2022-05-17 15:33:23 +0200

    Test updates

--- src/lib/extraParams.ts
+++ src/lib/extraParams.ts
@@ -1,4 +1,4 @@
 export interface ExtraParam {
     key: string;
-    value: string;
+    value: any;
 }
--- test/integration/extensibilityTests.ts
+++ test/integration/extensibilityTests.ts
@@ -75,4 +75,81 @@ describe('ExtensibilityTests', () => {
                 'The extra parameter was not added to the authorization request URL')
         });
     })
+
+    it('Starting a login request with the OpenID Connect acr_values parameter should include it in the request URL', async () => {
+
+        const options = {
+            extraParams: [
+                {
+                    key: 'acr_values',
+                    value: 'acr_values',
+                },
+            ],
+        };
+
+        const response = await fetch(
+            `${oauthAgentBaseUrl}/login/start`,
+            {
+                method: 'POST',
+                headers: {
+                    origin: config.trustedWebOrigins[0],
+                    'content-type': 'application/json',
+                },
+                body: JSON.stringify(options),
+            },
+        )
+
+        assert.equal(response.status, 200, 'Incorrect HTTP status')
+        const body = await response.json()
+        const authorizationRequestUrl = body.authorizationRequestUrl as string
+        
+        expect(authorizationRequestUrl).contains(
+            `${options.extraParams[0].key}=${options.extraParams[0].value}`,
+            'The extra parameter was not added to the authorization request URL')
+    })
+
+    it('Starting a login request with the OpenID Connect claims parameter should include it in the request URL', async () => {
+
+        const claims = {
+            id_token: {
+                acr: {
+                    essential: true,
+                    values: [
+                        "acr_1", "acr_2"
+                    ]
+                }
+            }
+        };
+        const claimsText = JSON.stringify(claims);
+        console.log(claimsText);
+
+        const options = {
+            extraParams: [
+                {
+                    key: 'claims',
+                    value: claimsText,
+                },
+            ],
+        };
+
+        const response = await fetch(
+            `${oauthAgentBaseUrl}/login/start`,
+            {
+                method: 'POST',
+                headers: {
+                    origin: config.trustedWebOrigins[0],
+                    'content-type': 'application/json',
+                },
+                body: JSON.stringify(options),
+            },
+        )
+
+        assert.equal(response.status, 200, 'Incorrect HTTP status')
+        const body = await response.json()
+        const authorizationRequestUrl = body.authorizationRequestUrl as string
+
+        expect(authorizationRequestUrl).contains(
+            `${options.extraParams[0].key}=${encodeURIComponent(options.extraParams[0].value)}`,
+            'The extra parameter was not added to the authorization request URL')
+    })
 })
