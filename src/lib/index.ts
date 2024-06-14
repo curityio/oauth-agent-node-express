176d94f018c09d8d8511180e448634a1b0e5029d
gary-archer <garyarcher36@gmail.com>
2024-06-14 19:54:25 +0100

    Updates

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -56,4 +56,4 @@ export {
     getIDCookieName,
     getAuthCookieName,
     generateRandomString,
-}
+};
--- start.sh
+++ start.sh
@@ -8,6 +8,28 @@ cd "$(dirname "${BASH_SOURCE[0]}")"
 . ./deployment/environments/dev/oauthagent.env
 
 #
+# Install dependencies
+#
+if [ ! -d 'node_modules' ]; then
+  npm install
+  if [ $? -ne 0 ]; then
+    echo 'Problem encountered installing dependencies'
+    read -n 1
+    exit 1
+  fi
+fi
+
+#
+# Also download certificates for local development
+#
+./downloadcerts.sh
+if [ $? -ne 0 ]; then
+  echo 'Problem encountered downloading certificates'
+  read -n 1
+  exit 1
+fi
+
+#
 # Then run the OAuth agent
 #
 node --loader ts-node/esm --no-warnings src/server.ts
