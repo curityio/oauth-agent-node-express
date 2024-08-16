90c755d6fb979af9a22df3071b42f35e66ff7feb
gary-archer <garyarcher36@gmail.com>
2024-08-16 20:24:16 +0100

    Updates

--- src/validateExpressRequest.ts
+++ src/validateExpressRequest.ts
@@ -3,8 +3,6 @@ import {UnauthorizedException} from './lib/exceptions/index.js'
 
 export default function validateExpressRequest(req: express.Request) {
 
-    console.log(req.header('token-handler-version'))
-    console.log(req.header)
     if (req.header('token-handler-version') !== '1') {
         const error = new UnauthorizedException()
         error.logInfo = 'The request did not contain the required custom header'
